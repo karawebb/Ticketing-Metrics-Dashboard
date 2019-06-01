b147558b2faf51fc97e434b073a26b375947b3e4
Kara Webb <kwebb.creation@gmail.com>
2019-05-31 18:12:05 -0700

    first commit

---
+++ src/data/appData.js
@@ -0,0 +1,17 @@
+export const donutChartData = [
+  { 'foodType': 'Open Tickets', 'percentSold': 16.5 },
+  { 'foodType': 'Closed Tickets', 'percentSold': 24 },
+  { 'foodType': 'Urgent', 'percentSold': 13 },
+  { 'foodType': 'Unresolved', 'percentSold': 20 },
+  { 'foodType': 'Other', 'percentSold': 10 }
+];
+
+
+export const barChartQ4Months =['October', 'November', 'December'];
+export const barChartMonthlyPercentages = [
+  { name: 'Open Tickets', data: [14, 16, 19.5] },
+  { name: 'Closed Tickets', data: [24, 23.5, 24.5] },
+  { name: 'Urgent', data: [16, 18, 17] },
+  { name: 'Unresolved', data: [21.5, 20, 17] },
+  { name: 'Other', data: [7, 12, 11] },
+];
